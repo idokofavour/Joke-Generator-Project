dafee074cfb2502f9f9f7a22fd59a676f6ecc451
Idoko Favour <ojodomofavour2004@gmail.com>
2023-09-15 19:44:07 +0100

    commiting all files

---
+++ script.js
@@ -0,0 +1,22 @@
+const jokeEL = document.getElementById('joke');
+const jokeBtn = document.getElementById('joke-btn');
+
+const generateJoke = () => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if(this.status === 200) {
+                jokeEL.innerHTML = JSON.parse(this.responseText).value;
+            } else {
+                jokeEL.innerHTML = 'Somthing Went Wrong (Not Funny)' ;
+            }
+        }
+    }
+    xhr.send();
+}
+
+jokeBtn.addEventListener('click', generateJoke);
+document.addEventListener('DOMContentLoaded', generateJoke);
